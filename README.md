200775fca6f34c6dc2c143cc0782fc6f96b2020e
András Debreczeni <ade@facelift-bbt.com>
2020-12-07 16:43:54 +0100

    update curl to be silent

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # EXAMPLE
 
-    docker run -it --rm --volume "`pwd`:/data" --user `id -u`:`id -g` md-renderer:local etc/docker/DOCKER.md
-
+    docker run --env GIT_BASE_URL="https://git.example.com/" -it --rm --volume "`pwd`:/data" --user `id -u`:`id -g` debreczeniandras/markdown-renderer:latest docs/README.md
--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-jq --slurp --raw-input '{"text": "\(.)", "gfm": true}' < /data/"$1" | curl -s --header Content-Type:application/json --data @- "https://git.fanactivator.com/api/v4/markdown" | jq --raw-output '.html' | lynx -stdin -dont_wrap_pre -force_html -underscore -use_mouse
+jq --slurp --raw-input '{"text": "\(.)", "gfm": true}' < /data/"$1" | curl -s --header Content-Type:application/json --data @- "${GIT_BASE_URL}/api/v4/markdown" | jq --raw-output '.html' | lynx -stdin -dont_wrap_pre -force_html -underscore -use_mouse
